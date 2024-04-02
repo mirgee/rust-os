75b18cba98dbf5fcece340d52a244d01ec7d53d5
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-04-02 23:03:58 +0200

    Add VGA buffer abstraction
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
+#![feature(lazy_cell)]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
@@ -12,14 +13,9 @@ fn panic(_: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop{}
+    loop {}
 }
