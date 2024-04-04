72a86601e3491d1f72f39a524838d7a1ab719bf3
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-04-04 22:36:28 +0200

    Custom test harness + tests
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 #![no_std]
-#![feature(lazy_cell)]
+// Main function is usually called by a runtime. In Rust, crt0 creates a stack and places arguments
+// to the right registers, and invokes entrypoint of the Rust runtime (start), which only takes
+// care of printing backtrace on panic and stack overflow guards. This runtime calls main function.
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+// The custom test harness generates a "main" function which calls test runner, but we use no_mean
+// feature and so is ignored. We rename it to test_main and run from our own entrypoint for now.
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+use rust_os::println;
+
+// our existing panic handler
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+// Linker expects this name for entrypoint.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
