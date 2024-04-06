75d7ccaf6f53a099603db3e3559aac078af629f2
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-04-06 20:08:29 +0200

    Load TSS set CS regiter - double faults handled as expected
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,9 +1,11 @@
 use core::ptr::addr_of;
 
-use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable};
-use x86_64::VirtAddr;
-use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
+use x86_64::instructions::tables::load_tss;
+use x86_64::registers::segmentation::{Segment, CS};
+use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
+use x86_64::structures::tss::TaskStateSegment;
+use x86_64::VirtAddr;
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
@@ -27,14 +29,23 @@ lazy_static! {
 
     // GDT defines boundaries of memory areas (segments, e.g. stack, heap, code, OS-reserved, ...)
     // used by programs. Segmentation was superseded by paging.
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        gdt.append(Descriptor::kernel_code_segment());
-        gdt.append(Descriptor::tss_segment(&TSS));
-        gdt
+        let code_selector = gdt.append(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.append(Descriptor::tss_segment(&TSS));
+        (gdt, Selectors { code_selector, tss_selector })
     };
 }
 
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
+}
+
 pub fn init() {
-    GDT.load();
+    GDT.0.load();
+    unsafe {
+        CS::set_reg(GDT.1.code_selector);
+        load_tss(GDT.1.tss_selector)
+    }
 }
